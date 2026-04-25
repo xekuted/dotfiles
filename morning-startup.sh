8916a42cdd32ae07e3c00a67d43e2a5a725118f4
xekuted <exekutable@gmail.com>
2026-04-25 16:04:59 +0530

    updated startup

--- morning-startup.sh
+++ morning-startup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-echo "Starting morning workflow..."
-
 # Browser
 nohup brave-browser --new-window \
+  "https://upskill.tutedude.com/course/lecture-mernstack" \
   "https://techcrunch.com/" \
   "https://www.artificialintelligence-news.com/" \
   "https://arstechnica.com/" \
@@ -14,8 +13,10 @@ nohup brave-browser --new-window \
 # Obsidian (Flatpak)
 nohup flatpak run md.obsidian.Obsidian >/dev/null 2>&1 &
 
-# Neovim in terminal
-nohup flatpak run org.wezfurlong.wezterm start -- nvim /home/xek/Proj/fokus +"10split | terminal" \
+# Neovim in WezTerm
+nohup flatpak run org.wezfurlong.wezterm start --always-new-process -- nvim ~/Proj/fokus +"10split | terminal" \
   >/dev/null 2>&1 &
 
-echo "Done."
+# System update in a separate WezTerm
+nohup flatpak run org.wezfurlong.wezterm start --always-new-process -- bash -c "sudo dnf update -y && sudo dnf upgrade -y && sudo dnf autoremove" \
+  >/dev/null 2>&1 &
